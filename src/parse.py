f9ce553d1ed12aef4708c11f5e12cee87dfaf366
SergePolin <serge@polin-family.ru>
2023-07-13 01:10:23 +0300

    add .github/workflows/build-publish.yml

--- src/parse.py
+++ src/parse.py
@@ -1,46 +1,63 @@
+"""
+Module: parse.py
+Description: This module provides a Parser class to retrieve holiday names from calend.ru.
+"""
+
+import datetime
 import requests
 from bs4 import BeautifulSoup
-import datetime
 
 
 class Parser:
-    def __init__(self, url="https://www.calend.ru/holidays/", css_selector=".holidays .title", **kwargs):
-        """
-        :param url: url to parse
-        :param css_selector: css selector to extract holiday names
-        :param kwargs: additional parameters
+    """
+    Parser class for retrieving holiday names from calend.ru.
+    """
 
-        Constructor of Parser class with default parameters url and css_selector to parse holidays from calend.ru
+    def __init__(self, url="https://www.calend.ru/holidays/", css_selector=".holidays .title"):
+        """
+        Constructor of Parser class.
+
+        :param url: URL to parse
+        :param css_selector: CSS selector to extract holiday names
         """
         self.url = url
         self.css_selector = css_selector
 
     def get_today_date(self):
-        '''
+        """
         Returns today's date in format "YYYY-MM-DD"
-        '''
+        """
         today = datetime.date.today()
         return today.strftime("%Y-%m-%d")
 
     def get_holiday(self, date):
         """
+        Retrieves the holiday names for a given date.
 
-        :param date: date in format "YYYY-MM-DD"
-        :return: list of holiday names or None if holiday is not found
+        :param date: Date in format "YYYY-MM-DD"
+        :return: List of holiday names or None if holiday is not found
         """
-        response = requests.get(self.url + date + "/")
-        soup = BeautifulSoup(response.text, 'html.parser')
-        # extract holiday name from css selector
-        holiday = soup.select(self.css_selector)
-        # if holiday is not found, return None
-        if len(holiday) == 0:
+        full_url = self.url + date + "/"
+        try:
+            response = requests.get(full_url, timeout=5)  # Set a timeout value for the HTTP request
+            response.raise_for_status()  # Raise an exception for HTTP errors (e.g., 404)
+        except requests.exceptions.RequestException as ex:
+            print(f"Error occurred while retrieving {full_url}: {ex}")
             return None
-        # else return holiday names
-        return [h.text.strip() for h in holiday]
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Extract holiday names from css selector
+        holidays = soup.select(self.css_selector)
+        if not holidays:
+            return None
+        return [h.text.strip() for h in holidays]
 
 
 if __name__ == "__main__":
     parser = Parser()
-    today = parser.get_today_date()
-    holiday = parser.get_holiday(today)
-    print(today, holiday)
+    current_date = parser.get_today_date()
+    holiday = parser.get_holiday(current_date)
+    if holiday:
+        print(f"Holidays on {current_date}: {', '.join(holiday)}")
+    else:
+        print(f"No holidays found on {current_date}")
